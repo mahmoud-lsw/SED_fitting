972675c5114477a7eda8f0021b72e289de2035fd
ACCarnall <adamc@roe.ac.uk>
2015-11-09 12:24:23 +0000

    Mon  9 Nov 12:24:23 GMT 2015

--- .download_new.sh
+++ .download_new.sh
@@ -4,6 +4,13 @@ echo Downloading latest version
 
 now=$(date)
 
-cp . ../backup_SED_fitting
-
-git pull origin master
+read -r -p "Are you sure? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        rm -r ../backup_SED_fitting/*
+        cp . ../backup_SED_fitting
+        git pull origin master
+        ;;
+    *)
+        ;;
+esac
