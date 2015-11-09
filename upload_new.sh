f0d9d402fd7b40104ecb5ea9b67257ed9267e0e4
ACCarnall <adamc@roe.ac.uk>
2015-11-09 10:59:25 +0000

    Mon  9 Nov 10:59:25 GMT 2015

--- upload_new.sh
+++ upload_new.sh
@@ -8,3 +8,16 @@ git add .
 git commit -m "$now"
 git push origin master
 
+# initial set up of repository:
+# first go to the github website, login and set up a new repository
+# go into the directory you want to put into the repository and type: git remote add origin https://github.com/ACCarnall/name_of_repository.git
+#
+# set up copies of repository:
+# start in folder in which you want the repository folder to be located (as a subfolder)
+# type: git clone git@github.com:ACCarnall/name_of_repository.git
+
+# There's a whole bunch of complex stuff about SSH keys if you're setting up from a new computer. This guide tells you how to do it:
+# https://help.github.com/articles/generating-ssh-keys/
+#
+# also useful:
+# http://stackoverflow.com/questions/6012073/how-do-i-code-against-one-github-repo-on-2-computers
