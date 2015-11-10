189a2476c53aa380efb734c6dd697bb028e352b9
ACCarnall <adamc@roe.ac.uk>
2015-11-10 19:59:34 +0000

    Tue 10 Nov 2015 19:59:34 GMT

--- analyse_output.py
+++ analyse_output.py
@@ -2,7 +2,8 @@ import numpy as np
 import pylab
 import matplotlib as mpl
 
-data = np.loadtxt("photoz.txt")
+#data = np.loadtxt("photoz/photoz_full_set_no_offsets.txt")
+data = np.loadtxt("photoz/photoz_allbandobj_v3.txt")
 
 ### Find the number of data points, catastrophic outliers and sigma_dz
 no = 0.
@@ -13,9 +14,6 @@ sig_dz = np.std(dz)
 for i in range(len(data)):
     if data[i,1] > 0.:
         no = no+1.
-
-print "Including catastrophic outliers" 
-print no, cat, sig_dz
 
 ### Remove catastrophic outliers from data sample: returns wellfit as the clean version of data
 gooddata = []
@@ -32,6 +30,15 @@ wellfit.shape = (len(gooddata), 8)
 for i in range(len(gooddata)):
     wellfit[i, :] = data[gooddata[i], :]
 
+print "Including catastrophic outliers" 
+print no, cat, sig_dz
+
+print "No catastrophic outliers" 
+print len(wellfit), 0., np.std((wellfit[:,1] - wellfit[:,2])/(1+wellfit[:,1]))
+
+MAD = np.median(np.abs(dz - np.median(dz)))
+print "sigma_NMAD: " + str(MAD/1.483)
+
 ### Calculate star formation rates and stellar masses for the full sample and for the well fit sample
 SFR = (1./(data[:,4]*10**9))*np.exp(-data[:,3]/(data[:,4]*10**9))*data[:,6]
 mass = (1. - np.exp(-data[:,3]/(data[:,4]*10**9)))*data[:,6]
@@ -41,7 +48,7 @@ good_mass = (1. - np.exp(-wellfit[:,3]/(wellfit[:,4]*10**9)))*wellfit[:,6]
 
 
 ### Various plotting codes
-"""
+
 maxxy = np.max([np.max(data[:,1]), np.max(data[:,2])]) + 0.05
 pylab.figure()
 pylab.scatter(data[:,1], data[:,2], c = data[:,3], norm=mpl.colors.LogNorm())
@@ -55,7 +62,7 @@ pylab.xlabel("spec_z")
 cbar = pylab.colorbar()
 cbar.set_label("Age of Stellar Pop. (yrs)", size=16)
 pylab.show()
-
+"""
 pylab.figure()
 pylab.scatter(data[:, 3]*10**-9, data[:,5], color="red")
 pylab.scatter(wellfit[:, 3]*10**-9, wellfit[:,5], color="blue")
