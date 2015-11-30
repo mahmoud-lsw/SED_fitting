af85cfe7413ae3493ae5afc61b46c12ec14dc13c
ACCarnall <adamc@roe.ac.uk>
2015-11-30 13:54:30 +0000

    Mon 30 Nov 13:54:30 GMT 2015

--- VANDELS/addcoord.py
+++ VANDELS/addcoord.py
@@ -1,7 +1,6 @@
 import numpy as np
 from astropy.io import fits
 
-<<<<<<< HEAD
 basetable = np.loadtxt("../../VANDELS_data/list.txt", dtype="str")
 
 finaltable = open("../../VANDELS_data/list_extra.txt", "w")
@@ -18,12 +17,3 @@ for i in range(len(basetable)):
     finaltable.write(basetable[i,0] + " " + str(wavzpt) + " " +  " " + str(dwav) + " " + str(ra) + " " + str(dec) + " " + str(ID) + "\n")
 
 finaltable.close()
-
-=======
-basetable = np.loadtxt("list.txt", dtype="str")
-
-for i in range(len(basetable)):
-    print "spectra/"+basetable[i,0]
-    hdulist = fits.open("spectra/"+basetable[i,0])
->>>>>>> d5718e00aa7a6eb657d835ca47c636fced7ba1e6
-
