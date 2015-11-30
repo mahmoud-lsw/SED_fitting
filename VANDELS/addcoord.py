f92f1b4b9bb498b86d2c8e575145e4419eef4260
ACCarnall <adamc@roe.ac.uk>
2015-11-30 13:52:11 +0000

    Mon 30 Nov 13:52:11 GMT 2015

--- VANDELS/addcoord.py
+++ VANDELS/addcoord.py
@@ -1,9 +1,21 @@
 import numpy as np
 from astropy.io import fits
 
-basetable = np.loadtxt("list.txt", dtype="str")
+basetable = np.loadtxt("../../VANDELS_data/list.txt", dtype="str")
+
+finaltable = open("../../VANDELS_data/list_extra.txt", "w")
+finaltable.write("# fname wavzpt dwav ra dec ID \n")
 
 for i in range(len(basetable)):
-    print "spectra/"+basetable[i,0]
-    hdulist = fits.open("spectra/"+basetable[i,0])
+    hdulist = fits.open("../../VANDELS_data/spectra/"+basetable[i,0])
+
+    wavzpt = hdulist[0].header["CRVAL1"]
+    dwav = hdulist[0].header["CDELT1"]
+    ra = hdulist[0].header["HIERARCH PND OBJRA"]
+    dec = hdulist[0].header["HIERARCH PND OBJDEC"]
+    ID = hdulist[0].header["HIERARCH PND OBJID"]
+    finaltable.write(basetable[i,0] + " " + str(wavzpt) + " " +  " " + str(dwav) + " " + str(ra) + " " + str(dec) + " " + str(ID) + "\n")
+
+finaltable.close()
+
 
